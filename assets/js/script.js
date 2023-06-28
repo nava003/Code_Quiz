115bb53904e5a3c1bf83e832852cdcb0eeddfe17
NavaHub <navaa003@gmail.com>
2023-06-28 01:59:45 -0500

    Readjusting html structure layout while inputting temp elements; further style layout via css; minor additions to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
-var introDisplay = document.querySelector('');
-var quizDisplay = document.querySelector('');
-var quizList = document.querySelector('');
+// ID Selection Declarations
+var introDisplay = document.querySelector('#introCard');
+var quizDisplay = document.querySelector('#quizCard');
+var quizList = document.querySelector('#quizList');
+var quizTimer = document.querySelector('#quizTimer');
 
+// Variable Declarations
 var secRemain = 120;
+
+// Style Initalization
+introDisplay.setAttribute("style", "display:none");
+quizDisplay.setAttribute("style", "display:flex");
+
 
