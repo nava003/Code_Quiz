2078ef0e44dd91921f87afc38c0f6ef2b20da786
NavaHub <navaa003@gmail.com>
2023-06-29 16:22:33 -0500

    Removing placeholders in html; additional visual placeholders in css; added and tested quiz timer functionality and working on QnA declaration in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,47 @@
 // ID Selection Declarations
 var introDisplay = document.querySelector('#introCard');
 var quizDisplay = document.querySelector('#quizCard');
-var quizList = document.querySelector('#quizList');
+var questDisplay = document.querySelector('#askQuestion')
+var ansDisplay = document.querySelector('#quizList');
 var quizTimer = document.querySelector('#quizTimer');
+var startBtn = document.querySelector('#startBtn');
 
 // Variable Declarations
-var secRemain = 120;
+var secRemain = 121;
+
+// Object-Array Declarations
+var questionsList = [{
+
+}];
+var answersList = [{
+
+}];
 
 // Style Initalization
-introDisplay.setAttribute("style", "display:none");
-quizDisplay.setAttribute("style", "display:flex");
+quizTimer.setAttribute("style", "display:none");
+quizDisplay.setAttribute("style", "display:none");
+
+function startTimer() {
+    quizTimer.setAttribute("style", "display:block");
+
+    var timeInterval = setInterval(function() {
+        secRemain--;
+        quizTimer.textContent = "Time Remaining: " + secRemain;
+        if(secRemain === 0) {
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+}
+
+function startQuiz() {
+    introDisplay.setAttribute("style", "display:none");
+    quizDisplay.setAttribute("style", "display:flex;justify-content: space-between;align-items: flex-start;");
 
 
+};
+
+// Button Listener
+startBtn.addEventListener('click', function() {
+    startTimer();
+    startQuiz();
+});
