83e08c956c24d7e2378d3e026e5b3c819ae77606
NavaHub <navaa003@gmail.com>
2023-07-04 17:18:25 -0500

    ID name change in html; readjusting styles in css; implemented an object to carry arrays of QnA objects, created elements to be appended and display expected content in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,32 +2,72 @@
 var introDisplay = document.querySelector('#introCard');
 var quizDisplay = document.querySelector('#quizCard');
 var questDisplay = document.querySelector('#askQuestion')
-var ansDisplay = document.querySelector('#quizList');
+var ansDisplay = document.querySelector('#ansList');
 var quizTimer = document.querySelector('#quizTimer');
 var startBtn = document.querySelector('#startBtn');
-
-// Variable Declarations
-var secRemain = 121;
-
-// Object-Array Declarations
-var questionsList = [{
-
-}];
-var answersList = [{
-
-}];
 
 // Style Initalization
 quizTimer.setAttribute("style", "display:none");
 quizDisplay.setAttribute("style", "display:none");
 
+// Variable Declarations
+var secRemain = 121;
+
+// Object-Array Declaration of QnA
+var QnAList = [
+    {
+        question: "When declaring variables in JavaScript, which of the following is correct?",
+        answers: [
+            "var",
+            "declare",
+            "$",
+            "have"
+        ],
+        correctAns: 1
+    },
+    {
+        question: "When inspecting a webpage in HMTL, where does most of the content appear in?",
+        answers: [
+            "[html]",
+            "[head]",
+            "[body]",
+            "[footer]"
+        ],
+        correctAns: 3
+    },
+    {
+        question: "In CSS, how do you center a text?",
+        answers: [
+            "content: center",
+            "text: center",
+            "align: center",
+            "text-align: center"
+        ],
+        correctAns: 4
+    },
+    {
+        question: "What does API stand for?",
+        answers: [
+            "Applying Program Interaction",
+            "Application Programming Interface",
+            "Application Processing Interface",
+            "Accessing Preferences Interface"
+        ],
+        correctAns: 2
+    },
+    {
+        question: "Is Documentation important?",
+        answers: ["Yes", "No"],
+        correctAns: 1
+    }];
+
 function startTimer() {
     quizTimer.setAttribute("style", "display:block");
 
-    var timeInterval = setInterval(function() {
+    var timeInterval = setInterval(function () {
         secRemain--;
         quizTimer.textContent = "Time Remaining: " + secRemain;
-        if(secRemain === 0) {
+        if (secRemain === 0) {
             clearInterval(timeInterval);
         }
     }, 1000);
@@ -37,11 +77,21 @@ function startQuiz() {
     introDisplay.setAttribute("style", "display:none");
     quizDisplay.setAttribute("style", "display:flex;justify-content: space-between;align-items: flex-start;");
 
+    var orderList = document.createElement('ol');
+    ansDisplay.append(orderList);
 
+    var questCount = 0;
+    // Display Question w/ suggested answers
+    questDisplay.textContent = "Question " + (questCount + 1) + ": " + QnAList[questCount].question;
+    for (var i = 0; i < QnAList[questCount].answers.length; i++) {
+        var listItem = document.createElement('li');
+        listItem.textContent = QnAList[questCount].answers[i];
+        orderList.append(listItem);
+    }
 };
 
 // Button Listener
-startBtn.addEventListener('click', function() {
+startBtn.addEventListener('click', function () {
     startTimer();
     startQuiz();
 });
